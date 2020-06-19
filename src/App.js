aae08fad0166353381d2198a602d7d1f072213b3
Jagdish <jatin6972@gmail.com>
2020-06-19 08:10:11 +0530

    add CartItem component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import CartItem from "./CartItem.js";
 function App() {
   const name = "Jatin";
   const isLoggedIn = true;
 
   return (
     <div className="App">
-      <h1>Cart</h1>
+      <CartItem />
     </div>
   );
 }
